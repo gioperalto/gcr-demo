5f1ec4d9f2c52a6224c7727028f3e7e0089e5724
gioperalto <gyoperalto@gmail.com>
2024-12-18 11:19:05 -0500

    Added pub-sub logsink

---
+++ datadog_log.tf
@@ -0,0 +1,29 @@
+resource "google_pubsub_topic" "export-logs-to-datadog" {
+  name = "export-logs-to-datadog"
+}
+
+resource "google_pubsub_subscription" "datadog-logs" {
+  name  = "datadog-logs"
+  topic = google_pubsub_topic.export-logs-to-datadog.name
+
+  message_retention_duration = "604800s"
+  retain_acked_messages      = false
+  ack_deadline_seconds       = 60
+
+  push_config {
+    push_endpoint = "https://gcp-intake.logs.datadoghq.com/v1/input/${var.datadog_key}/"
+  }
+}
+
+resource "google_logging_project_sink" "datadog-sink" {
+  name                   = "datadog-sink"
+  destination            = "pubsub.googleapis.com/${google_pubsub_topic.export-logs-to-datadog.id}"
+  filter                 = ""
+  unique_writer_identity = true
+}
+
+resource "google_project_iam_member" "pubsub-publisher-permisson" {
+  role   = "roles/pubsub.publisher"
+  member = google_logging_project_sink.datadog-sink.writer_identity
+  project = var.project
+}
---
+++ variable.tf
@@ -0,0 +1,11 @@
+# variable.tf 
+
+variable "datadog_key" {
+   type = string
+   description = "Datadog API Key"
+}
+
+variable "project" {
+   type = string
+   description = "GCP project"
+}
